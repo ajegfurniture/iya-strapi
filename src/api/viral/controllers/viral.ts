e56098b6eaa80f3fd3b1d543dbf43532ccd4ee71
Ajeg.co.id <ajeg.mebel@gmail.com>
2025-09-04 16:35:29 +0700

    feat: add author, category, tag, and viral content types with controllers, routes, and services
    - Created author content type with attributes: fullName, slug, avatar, bio, and virals.
    - Implemented author controller, router, and service.
    - Created category content type with attributes: name, slug, description, and virals.
    - Implemented category controller, router, and service.
    - Created tag content type with attributes: name, slug, and virals.
    - Implemented tag controller, router, and service.
    - Created viral content type with attributes: title, slug, cover, content, publishDate, readingTime, author, category, and tags.
    - Implemented viral controller, router, and service.
    - Updated generated TypeScript definitions for new content types and attributes.

---
+++ src/api/viral/controllers/viral.ts
@@ -0,0 +1,7 @@
+/**
+ * viral controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::viral.viral');
---
+++ src/api/viral/services/viral.ts
@@ -0,0 +1,7 @@
+/**
+ * viral service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::viral.viral');
